20ccbfd96f6d1d3ecf1987058acea7fc5664254f
Greg Brosman <greg.brosman@gmail.com>
2020-07-27 17:17:32 -0700

    refactored

--- item.py
+++ item.py
@@ -4,22 +4,32 @@ class Item():
         self.price = price
         self.quantity = quantity
 
-items_list = []
+
+def item_count():
+    answer = input("Would you like to add an item?").lower()
+    if answer == "yes":
+        items = add_item_to_list()   
+        item_count()     
+    elif answer == "no":
+        print("ok")
+    else:
+        print("I didn't recognize that.")
+        item_count()        
+        
 
 def add_item_to_list():
+    items_list = []
     name_of_item = input("What is the name of the item?")
     price_of_item = float(input("What is the price of the item?"))
     quantity_of_item = int(input("How many are you buying?"))
     new_item = Item(name_of_item, price_of_item, quantity_of_item)
-    return new_item
+    items_list.append(new_item)    
+    return items_list
 
 def total_cost(item):
     total = item.price * item.quantity
+    print(f"The item's total cost is: ${total}.")
     return total
 
 
-items_list.append(add_item_to_list())
-print(f"The first item's total cost is: ${total_cost(items_list[0])}.")
-
-items_list.append(add_item_to_list())
-print(f"The second item's total cost is: ${total_cost(items_list[1])}.")
+item_count()
