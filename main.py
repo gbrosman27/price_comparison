3d352411c8f868bb7f79da121405b007be8bd0b1
Greg Brosman <greg.brosman@gmail.com>
2020-11-01 13:40:05 -0800

    tkinter format, currency format

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from tkinter import *
-from tkinter import font
-from tkinter.font import BOLD, families
+import tkinter as tk
 
 
 class Item:
@@ -15,9 +13,9 @@ def item_count():
     items = None
     while True:
         answer = input("Would you like to add an item (yes or no)?  ").lower()
-        if answer == "yes":
+        if answer == "yes" or answer == "y":
             items = add_item_to_list(items_list)   
-        elif answer == "no":
+        elif answer == "no" or answer == "n":
             print("ok")
             break
         else:
@@ -28,8 +26,8 @@ def item_count():
 def add_item_to_list(items_list):
     try:
         name_of_item = input("What is the name of the item?  ")
-        price_of_item = float(input("What is the price of the item?  "))
-        servings_of_item = int(input("How many servings are in the item?  "))
+        price_of_item = float(input("What is the price of the item? (x.xx)  "))
+        servings_of_item = int(input("How many servings are in the item? (x)  "))
         new_item = Item(name_of_item, price_of_item, servings_of_item)
         items_list.append(new_item)  
         return items_list  
@@ -40,18 +38,18 @@ def add_item_to_list(items_list):
 def cost_per_unit(item):
     cost_list = []
     for x in item:
-        total = round(x.price / x.servings, 2)
-        result = f"The {x.name} cost per serving is: ${total}."
+        total = format(x.price / x.servings, ".2f")
+        result = f"The {x.name} cost per serving is: ${total}"
         cost_list.append(result)
     gui_kinter(cost_list)
 
+
 def gui_kinter(display_string_list):
-    root = Tk()
-    text = Text(root, spacing1=2, font=BOLD)
+    root = tk.Tk()    
     # loop over the list and convert to string format with new line.
     convert_list = '\n\n'.join(map(str, display_string_list)) 
-    text.insert(INSERT, convert_list)
-    text.pack()
+    label = tk.Label(root, text=convert_list, fg="blue", font="Arial 16 bold")
+    label.pack(fill=tk.X, padx=10, pady=10)
     root.mainloop()
 
 
